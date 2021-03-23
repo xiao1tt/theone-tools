a652c8fa230e4c052444bc2e619f8be749b074e8
chen <xiao1tt@icloud.com>
2021-03-23 16:33:49 +0800

    chen update

--- sso-client/src/main/java/com/theone/tools/sso/client/BaseSsoFilter.java
+++ sso-client/src/main/java/com/theone/tools/sso/client/BaseSsoFilter.java
@@ -21,6 +21,7 @@ public abstract class BaseSsoFilter implements Filter {
 
     }
 
+    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
